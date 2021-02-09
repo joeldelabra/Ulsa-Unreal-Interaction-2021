92a3628ec15f10d711f4a59148906c4af39aafca
joeldelabra <joeldelabra@gmail.com>
2021-02-09 13:52:51 -0700

    Video clases Unreal 2021

---
+++ Source/UlsaUEInteractions/UlsaUEInteractionsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UlsaUEInteractionsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ULSAUEINTERACTIONS_API AUlsaUEInteractionsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
